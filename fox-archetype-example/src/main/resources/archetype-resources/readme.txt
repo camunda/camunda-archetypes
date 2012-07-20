3b29f2771af2da3f1c21a45e1a3b869def5ccb00
Falko Menge <falko.menge@camunda.com>
2012-07-20 19:14:20 +0200

    HEMERA-2463: Added archetype for examples

---
+++ fox-archetype-example/src/main/resources/archetype-resources/readme.txt
@@ -0,0 +1,16 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+${symbol_pound} Introduction
+This example shows a way to implement ... 
+
+${symbol_pound} Environment Restrictions
+
+${symbol_pound} Remarks to run this example
+There is no web interface to access the application. To get started refer to the
+Arquillian test case, which by default connects to a fox platform running
+locally on JBoss AS 7.
+
+${symbol_pound} Known Issues
+
+${symbol_pound} Improvements Backlog
---
+++ fox-archetype-example/src/main/resources/archetype-resources/src/test/java/ArquillianProcessExecutionTest.java
@@ -0,0 +1,79 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.runtime.ProcessInstance;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.jboss.shrinkwrap.resolver.api.DependencyResolvers;
+import org.jboss.shrinkwrap.resolver.api.maven.MavenDependencyResolver;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(Arquillian.class)
+public class ArquillianProcessExecutionTest {
+  
+  private static final String PROCESS_DEFINITION_KEY = "${artifactId}";
+
+  @Deployment
+  public static WebArchive createDeployment() {
+    MavenDependencyResolver resolver = DependencyResolvers.use(MavenDependencyResolver.class)
+      .goOffline()
+      .loadMetadataFromPom("pom.xml");
+    
+    // if you experience problems with the authentication to the camunda fox
+    // repository the wrong maven configuration might be used.
+    // use this code to use your maven settings.xml in this case:
+    // .configureFrom(".../settings.xml")
+
+    return ShrinkWrap
+            .create(WebArchive.class, "${artifactId}.war")
+            // prepare as process application archive for fox platform
+            .addAsLibraries(resolver.artifact("com.camunda.fox.platform:fox-platform-client").resolveAsFiles())
+            .addAsWebResource("META-INF/processes.xml", "WEB-INF/classes/META-INF/processes.xml")
+            .addAsWebResource("META-INF/beans.xml", "WEB-INF/classes/META-INF/beans.xml")
+            // add your own classes (could be done one by one as well)
+            .addPackages(false, "${package}")
+            // add process definition
+            .addAsResource("process.bpmn")
+    // now you can add additional stuff required for your test case
+    ;
+  }
+
+  @Inject
+  private ProcessEngine processEngine;
+
+  /**
+   * Tests that a user cannot claim or complete a task if he completed the first
+   * task
+   */
+  @Test
+  public void testInvalidClaim() throws Exception {
+    cleanUpRunningProcessInstances();
+    
+    ProcessInstance processInstance = processEngine.getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+
+    assertEquals(1, processEngine.getHistoryService().createHistoricProcessInstanceQuery().processInstanceId(processInstance.getId()).finished().count());
+  }
+
+  /**
+   * Helper to delete all running process instances which might disturb our Arquillian Test case
+   * Better run test cases in a clean environment, but this is pretty handy for demo purposes
+   */
+  private void cleanUpRunningProcessInstances() {
+    List<ProcessInstance> runningInstances = processEngine.getRuntimeService().createProcessInstanceQuery().processDefinitionKey(PROCESS_DEFINITION_KEY).list();
+    for (ProcessInstance processInstance : runningInstances) {
+      processEngine.getRuntimeService().deleteProcessInstance(processInstance.getId(), "deleted to have a clean environment for Arquillian");
+    }
+  }  
+}
