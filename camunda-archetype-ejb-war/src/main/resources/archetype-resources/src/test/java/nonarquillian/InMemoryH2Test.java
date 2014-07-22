3f8d12c582af7fd73095d462fd4cfc44c47baa12
Falko Menge <falko.menge@camunda.com>
2014-07-22 14:34:59 +0200

    Add camunda-bpm-assert and switch to JUnit 4 w/ Rule

--- camunda-archetype-ejb-war/src/main/resources/archetype-resources/src/test/java/nonarquillian/InMemoryH2Test.java
+++ camunda-archetype-ejb-war/src/main/resources/archetype-resources/src/test/java/nonarquillian/InMemoryH2Test.java
@@ -4,13 +4,20 @@
 package ${package}.nonarquillian;
 
 import org.camunda.bpm.engine.impl.util.LogUtil;
-import org.camunda.bpm.engine.test.ProcessEngineTestCase;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
 import org.camunda.bpm.engine.test.Deployment;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
 /**
  * Test case starting an in-memory database-backed Process Engine.
  */
-public class InMemoryH2Test extends ProcessEngineTestCase {
+public class InMemoryH2Test {
+
+  @Rule
+  public ProcessEngineRule processEngineRule = new ProcessEngineRule();
 
   private static final String PROCESS_DEFINITION_KEY = "${artifactId}";
 
@@ -22,6 +29,7 @@ public class InMemoryH2Test extends ProcessEngineTestCase {
   /**
    * Just tests if the process definition is deployable.
    */
+  @Test
   @Deployment(resources = "process.bpmn")
   public void testParsingAndDeployment() {
     // nothing is done here, as we just want to check for exceptions during deployment
