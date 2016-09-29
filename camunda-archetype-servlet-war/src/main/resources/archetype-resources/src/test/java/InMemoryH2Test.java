0a249082bd0b28163ef77c713bcd41fc96adfcf2
Falko Menge <falko.menge@camunda.com>
2016-09-29 15:29:21 +0200

    feat(process-test-coverage): upgrade to new test coverage extension

--- camunda-archetype-servlet-war/src/main/resources/archetype-resources/src/test/java/InMemoryH2Test.java
+++ camunda-archetype-servlet-war/src/main/resources/archetype-resources/src/test/java/InMemoryH2Test.java
@@ -6,24 +6,24 @@ package ${package};
 import org.apache.ibatis.logging.LogFactory;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
 import org.camunda.bpm.engine.test.Deployment;
-import org.junit.After;
 import org.junit.Before;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 import static org.junit.Assert.*;
 
-import org.camunda.bpm.consulting.process_test_coverage.ProcessTestCoverage;
-
 /**
  * Test case starting an in-memory database-backed Process Engine.
  */
 public class InMemoryH2Test {
 
+  @ClassRule
   @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+  public static ProcessEngineRule rule = TestCoverageProcessEngineRuleBuilder.create().build();
 
   private static final String PROCESS_DEFINITION_KEY = "${artifactId}";
 
@@ -51,17 +51,6 @@ public class InMemoryH2Test {
 	  //ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
 	  
 	  // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
-	  
-	  // To generate the coverage report for a single tests add this line as the last line of your test method:
-	  //ProcessTestCoverage.calculate(processInstance, rule.getProcessEngine());
   }
-
-  /**
-   * @see <a href="../../../../../../../target/process-test-coverage/${artifactId}.html">Process Test Coverage Report</a> 
-   */
-  @After
-  public void calculateCoverageForAllTests() throws Exception {
-    ProcessTestCoverage.calculate(rule.getProcessEngine());
-  }  
 
 }
