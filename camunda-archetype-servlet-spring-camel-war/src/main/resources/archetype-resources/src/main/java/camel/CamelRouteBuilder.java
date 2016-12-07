6b83f8f8306b4ef5d5fce83a4c11fa38e67cf3d6
Falko Menge <falko.menge@camunda.com>
2016-12-07 17:05:43 +0100

    Add route for writing a file

--- camunda-archetype-servlet-spring-camel-war/src/main/resources/archetype-resources/src/main/java/camel/CamelRouteBuilder.java
+++ camunda-archetype-servlet-spring-camel-war/src/main/resources/archetype-resources/src/main/java/camel/CamelRouteBuilder.java
@@ -7,7 +7,12 @@ public class CamelRouteBuilder extends RouteBuilder {
 
   @Override
   public void configure() throws Exception {
-    
+
+    from("direct:variableToFile")
+      .log(LoggingLevel.INFO, "Writing variable to file")
+      .process(new FirstMapValueToStringProcessor())
+      .to("file://" + System.getProperty("user.home") + System.getProperty("file.separator") + "camunda-bpm-demo-camel" + System.getProperty("file.separator") + "variableToFile");
+
 //    from("pop3://mail.example.com?username=camunda@example.com&password=camunda&delete=true&unseen=true&consumer.delay=60000")
 //      .log(LoggingLevel.INFO, "Received email. Starting process...")
 //      .process(new InvoiceMailProcessor())
@@ -16,6 +21,7 @@ public class CamelRouteBuilder extends RouteBuilder {
     from("direct:invoiceToDB")
       .log(LoggingLevel.INFO, "Sending invoice to DB");
 //      .to("sql:insert into INVOICE set foo=:#myProcessVariable?dataSource=dataSource");
+    
   }
 
 }
---
+++ camunda-archetype-servlet-spring-camel-war/src/main/resources/archetype-resources/src/main/java/camel/FirstMapValueToStringProcessor.java
@@ -0,0 +1,15 @@
+package ${package}.camel;
+
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+public class FirstMapValueToStringProcessor implements Processor {
+
+  public void process(Exchange exchange) throws Exception {
+    Map map = exchange.getIn().getBody(Map.class);
+    exchange.getIn().setBody(map.values().iterator().next().toString());
+  }
+
+}
