824d40fac9e350419aabf2e8c49516ea7ae88b97
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2017-12-20 16:44:09 +0100

    name the process application

--- camunda-archetype-spring-boot/src/main/resources/archetype-resources/src/main/java/CamundaApplication.java
+++ camunda-archetype-spring-boot/src/main/resources/archetype-resources/src/main/java/CamundaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableProcessApplication
+@EnableProcessApplication("${artifactId}")
 public class CamundaApplication {
   public static void main(String... args) {
     SpringApplication.run(CamundaApplication.class, args);
