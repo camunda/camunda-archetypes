f50147dfa4d17a6b9a532f3317853e880e0cc68a
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2018-01-11 09:48:50 +0100

    rename to execution again

--- camunda-archetype-spring-boot/src/main/resources/archetype-resources/src/main/java/LoggerDelegate.java
+++ camunda-archetype-spring-boot/src/main/resources/archetype-resources/src/main/java/LoggerDelegate.java
@@ -17,15 +17,15 @@ public class LoggerDelegate implements JavaDelegate {
  
   private final Logger LOGGER = Logger.getLogger(LoggerDelegate.class.getName());
   
-  public void execute(DelegateExecution context) throws Exception {
+  public void execute(DelegateExecution execution) throws Exception {
     
     LOGGER.info("\n\n  ... LoggerDelegate invoked by "
-            + "processDefinitionId=" + context.getProcessDefinitionId()
-            + ", activtyId=" + context.getCurrentActivityId()
-            + ", activtyName='" + context.getCurrentActivityName() + "'"
-            + ", processInstanceId=" + context.getProcessInstanceId()
-            + ", businessKey=" + context.getProcessBusinessKey()
-            + ", executionId=" + context.getId()
+            + "processDefinitionId=" + execution.getProcessDefinitionId()
+            + ", activtyId=" + execution.getCurrentActivityId()
+            + ", activtyName='" + execution.getCurrentActivityName() + "'"
+            + ", processInstanceId=" + execution.getProcessInstanceId()
+            + ", businessKey=" + execution.getProcessBusinessKey()
+            + ", executionId=" + execution.getId()
             + " \n\n");
   }
 
