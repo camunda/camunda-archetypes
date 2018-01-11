df8d5339b580b7cc927c070638aab292ad696e9a
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2018-01-11 11:00:35 +0100

    spring-boot-like process test

---
+++ camunda-archetype-spring-boot/src/main/resources/archetype-resources/src/test/java/ProcessTest.java
@@ -0,0 +1,49 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.apache.ibatis.logging.LogFactory;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
+import static org.junit.Assert.*;
+
+/**
+ * Test case starting an in-memory database-backed Process Engine.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.NONE)
+public class ProcessTest {
+
+  private static final String PROCESS_DEFINITION_KEY = "${artifactId}";
+
+  @Autowired
+  private ProcessEngine processEngine;
+
+  static {
+    LogFactory.useSlf4jLogging(); // MyBatis
+  }
+
+  @Before
+  public void setup() {
+    init(processEngine);
+  }
+
+  @Test
+  public void testHappyPath() {
+    // Either: Drive the process by API and assert correct behavior by camunda-bpm-assert, e.g.:
+    //ProcessInstance processInstance = processEngine().getRuntimeService().startProcessInstanceByKey(PROCESS_DEFINITION_KEY);
+    
+    // Now: Drive the process by API and assert correct behavior by camunda-bpm-assert
+  }
+
+}
