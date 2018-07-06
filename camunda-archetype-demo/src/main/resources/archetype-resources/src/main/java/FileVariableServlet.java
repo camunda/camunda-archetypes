4698ea28c7fea0d100d652e7fd473f330f127b73
Falko Menge <falko.menge@camunda.com>
2018-07-06 16:57:21 +0200

    Add fallback document to FileVariableServlet

--- camunda-archetype-demo/src/main/resources/archetype-resources/src/main/java/FileVariableServlet.java
+++ camunda-archetype-demo/src/main/resources/archetype-resources/src/main/java/FileVariableServlet.java
@@ -2,6 +2,7 @@ package ${package};
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,43 +22,50 @@ public class FileVariableServlet extends HttpServlet {
 
   private static final long serialVersionUID = 1L;
 
+  private final Logger LOGGER = Logger.getLogger(FileVariableServlet.class.getName());
+  
   @Override
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-    ProcessEngine processEngine = BpmPlatform.getDefaultProcessEngine();
-    String variableId = request.getParameter("variableId");
-    String taskId = request.getParameter("taskId");
-    String variableName = request.getParameter("variableName");
-
-    TypedValue typedValue = null;
-    if (variableId != null) {
-      typedValue = processEngine.getRuntimeService()
-        .createVariableInstanceQuery()
-        .variableId(variableId)
-        .singleResult()
-        .getTypedValue();
-    } else if (taskId != null && variableName != null) {
-      typedValue = processEngine.getTaskService()
-          .getVariableTyped(taskId, variableName);
-      if (typedValue == null) {
-        String processInstanceId = processEngine.getTaskService().createTaskQuery()
-          .taskId(taskId)
-          .singleResult()
-          .getProcessInstanceId();
+    try {
+      ProcessEngine processEngine = BpmPlatform.getDefaultProcessEngine();
+      String variableId = request.getParameter("variableId");
+      String taskId = request.getParameter("taskId");
+      String variableName = request.getParameter("variableName");
+  
+      TypedValue typedValue = null;
+      if (variableId != null) {
         typedValue = processEngine.getRuntimeService()
-            .createVariableInstanceQuery()
-            .processInstanceIdIn(processInstanceId)
-            .variableName(variableName)
+          .createVariableInstanceQuery()
+          .variableId(variableId)
+          .singleResult()
+          .getTypedValue();
+      } else if (taskId != null && variableName != null) {
+        typedValue = processEngine.getTaskService()
+            .getVariableTyped(taskId, variableName);
+        if (typedValue == null) {
+          String processInstanceId = processEngine.getTaskService().createTaskQuery()
+            .taskId(taskId)
             .singleResult()
-            .getTypedValue();
+            .getProcessInstanceId();
+          typedValue = processEngine.getRuntimeService()
+              .createVariableInstanceQuery()
+              .processInstanceIdIn(processInstanceId)
+              .variableName(variableName)
+              .singleResult()
+              .getTypedValue();
+        }
       }
-    }
-    if (typedValue instanceof FileValue) {
-      FileValue fileValue = (FileValue) typedValue;
-      response.setContentType(fileValue.getMimeType());
-      InputStream inputStream = fileValue.getValue();
+      if (typedValue instanceof FileValue) {
+        FileValue fileValue = (FileValue) typedValue;
+        response.setContentType(fileValue.getMimeType());
+        InputStream inputStream = fileValue.getValue();
+        IOUtils.copy(inputStream, response.getOutputStream());
+      }
+    } catch (Exception e) {
+      LOGGER.warning(e.getMessage());
+      InputStream inputStream = this.getClass().getResourceAsStream("/document.pdf");
+      response.setContentType("application/pdf");
       IOUtils.copy(inputStream, response.getOutputStream());
     }
-
   }
 }
