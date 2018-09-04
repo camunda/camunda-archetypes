e598efc7dd580661a828bd749be4b9bd9fb9362d
Falko Menge <falko.menge@camunda.com>
2018-09-04 09:37:08 +0200

    Add Spring Boot Demo archetype (copy of Spring Boot archetype)

---
+++ camunda-archetype-spring-boot-demo/src/main/resources/archetype-resources/src/test/java/ProcessTestCoverageProcessEnginePlugin.java
@@ -0,0 +1,20 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.camunda.bpm.engine.impl.cfg.AbstractProcessEnginePlugin;
+import org.camunda.bpm.engine.impl.cfg.ProcessEngineConfigurationImpl;
+import org.camunda.bpm.engine.impl.cfg.ProcessEnginePlugin;
+import org.camunda.bpm.extension.process_test_coverage.junit.rules.ProcessCoverageConfigurator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProcessTestCoverageProcessEnginePlugin extends AbstractProcessEnginePlugin implements ProcessEnginePlugin {
+
+  @Override
+  public void preInit(ProcessEngineConfigurationImpl processEngineConfiguration) {
+    ProcessCoverageConfigurator.initializeProcessCoverageExtensions(processEngineConfiguration);
+  }
+
+}
