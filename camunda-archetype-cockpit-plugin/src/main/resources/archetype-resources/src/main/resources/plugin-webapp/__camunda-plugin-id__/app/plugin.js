c4f39c8ba2a57eb66d9869e5e128b378a5ad6892
Martin Stamm <martin.stamm@camunda.com>
2020-10-29 12:39:24 +0100

    chore(): migrate frontend plugin to 7.14

--- camunda-archetype-cockpit-plugin/src/main/resources/archetype-resources/src/main/resources/plugin-webapp/__camunda-plugin-id__/app/plugin.js
+++ camunda-archetype-cockpit-plugin/src/main/resources/archetype-resources/src/main/resources/plugin-webapp/__camunda-plugin-id__/app/plugin.js
@@ -1,36 +1,42 @@
-#set ($symbol_dollar = '$') 
-define(['angular'], function(angular) {
+function generateHTML(processInstanceCounts) {
+  const body = processInstanceCounts
+    .map(
+      element => `<tr>
+                    <td>${element.key}</td>
+                    <td>${element.instanceCount}</td>
+                  </tr>`
+    )
+    .join("\n");
 
-  var ngModule = angular.module('cockpit.plugin.${camunda-plugin-id}', []);
+  return `
+<section>
+  <div class="inner">
+    <h3>${camunda-plugin-name}</h3>
+    <table class="table table-bordered table-hover table-condensed">
+      <thead>
+        <tr>
+          <th>Key</th>
+          <th>Instances</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${body}
+      </tbody>
+    </table>
+  </div>
+</section>`;
+}
 
-  var DashboardController = function($scope, $http, Uri) {
-
-    ${symbol_dollar}http.get(Uri.appUri("plugin://${camunda-plugin-id}/:engine/process-instance"))
-      .success(function(data) {
-        $scope.processInstanceCounts = data;
-      });
-  };
-
-  DashboardController.$inject = ["$scope", "$http", "Uri"];
-
-
-  var Configuration = function Configuration(ViewsProvider) {
-
-    ViewsProvider.registerDefaultView('cockpit.processes.dashboard', {
-      id: '${camunda-plugin-id}',
-      label: '${camunda-plugin-name}',
-      url: 'plugin://${camunda-plugin-id}/static/app/dashboard.html',
-      controller: DashboardController,
-
-      // make sure we have a higher priority than the default plugin
-      priority: 12
+export default {
+  id: "${camunda-plugin-id}",
+  pluginPoint: "cockpit.processes.dashboard",
+  priority: 12,
+  render: (container, { cockpitApi, engine }) => {
+    fetch(
+      `${cockpitApi}/plugin/${camunda-plugin-id}/${engine}/process-instance`
+    ).then(async res => {
+      const processInstanceCounts = await res.json();
+      container.innerHTML = generateHTML(processInstanceCounts);
     });
-  };
-
-  Configuration.$inject = ['ViewsProvider'];
-
-  ngModule.config(Configuration);
-  ngModule.controller('DashboardController', [ '$scope' ]);
-
-  return ngModule;
-});
+  }
+};
