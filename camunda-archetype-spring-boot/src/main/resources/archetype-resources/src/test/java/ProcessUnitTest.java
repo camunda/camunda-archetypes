735d4fdbf1e00d3b4d43f2b7743d3f4b717149f1
Ingo Richtsmeier <ingo.richtsmeier@camunda.com>
2023-02-16 09:31:35 +0100

    Use JUnit5 for Spring Boot Process Application

--- camunda-archetype-spring-boot/src/main/resources/archetype-resources/src/test/java/ProcessUnitTest.java
+++ camunda-archetype-spring-boot/src/main/resources/archetype-resources/src/test/java/ProcessUnitTest.java
@@ -3,53 +3,31 @@
 #set( $symbol_escape = '\' )
 package ${package};
 
-import javax.annotation.PostConstruct;
-
-import org.apache.ibatis.logging.LogFactory;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.camunda.bpm.engine.test.Deployment;
-import org.camunda.bpm.engine.test.ProcessEngineRule;
-import org.camunda.bpm.extension.process_test_coverage.junit.rules.TestCoverageProcessEngineRuleBuilder;
-import org.junit.Before;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.camunda.community.process_test_coverage.spring_test.platform7.ProcessEngineCoverageConfiguration;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.context.annotation.Import;
 
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-import static org.junit.Assert.*;
+import static org.camunda.bpm.engine.test.assertions.bpmn.BpmnAwareTests.*;
 import static org.assertj.core.api.Assertions.*;
 
 
 /**
  * Test case starting an in-memory database-backed Process Engine.
  */
-@RunWith(SpringRunner.class)
 @SpringBootTest
+@Import(ProcessEngineCoverageConfiguration.class)
 public class ProcessUnitTest {
 
   @Autowired
   private ProcessEngine processEngine;
 
-  static {
-    LogFactory.useSlf4jLogging(); // MyBatis
-  }
-
-  @ClassRule
-  @Rule
-  public static ProcessEngineRule rule;
-
-  @PostConstruct
-  void initRule() {
-    rule = TestCoverageProcessEngineRuleBuilder.create(processEngine).build();
-  }
-
-  @Before
+  @BeforeEach
   public void setup() {
     init(processEngine);
   }
